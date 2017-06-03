38f1547c95a43f4270f940c4f15dfc973434cf27
anditakaesar <anditakaesar@gmail.com>
2017-06-03 16:52:40 +0700

    Editing models and views. Giving active field and set the create_date to more specific time

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from aoms import settings
 import datetime
 
 
@@ -6,17 +7,36 @@ import datetime
 class BlogPost(models.Model):
     post_title = models.CharField(max_length=1000)
     post_body = models.TextField()
-    created_date = models.DateField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.post_title
 
 
 class Navigation(models.Model):
+    TARGET_CHOICES = (
+        ('_blank', 'BLANK'),
+        ('_parent', 'PARENT'),
+        ('_self', 'SELF'),
+        ('_top', 'TOP'),
+    )
+
     nav_title = models.CharField(max_length=50)
     nav_desc = models.CharField(max_length=200)
     nav_location = models.CharField(max_length=1000)
+    nav_target = models.CharField(max_length=10, choices=TARGET_CHOICES)
+    is_relative = models.BooleanField(default=True)
+    active = models.BooleanField(default=True)
+    nav_priority = models.SmallIntegerField(default=50)
+
+    @property
+    def nav_reff(self):
+        reff = self.nav_location
+        if self.is_relative == 1:
+            reff = settings.BASE_URL + self.nav_location
+        return reff
 
     def __str__(self):
-        return self.nav_title + " : " + self.nav_desc
+        return self.nav_title + " -> " + self.nav_reff
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from blog.models import BlogPost, Navigation
-from django.utils import timezone
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+    post_lists = BlogPost.objects.order_by('-created_date').filter(active=1)
+    nav_lists = Navigation.objects.order_by('nav_priority').filter(active=1)
 
     def get_context_data(self, **kwargs):
+        postpaginator = Paginator(self.post_lists, 3)  # show $ contacts per page
+        page = self.request.GET.get('page')
+
+        try:
+            posts = postpaginator.page(page)
+        except PageNotAnInteger:
+            posts = postpaginator.page(1)
+        except EmptyPage:
+            posts = postpaginator.page(postpaginator.num_pages)
+
         context = super(HomePageView, self).get_context_data(**kwargs)
-        context['nav_lists'] = Navigation.objects.all()
+        context['nav_lists'] = self.nav_lists
+        context['post_lists'] = posts
         return context
