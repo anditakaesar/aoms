d7bf8efa46adae87084ef013a92ae538da60e866
Andita Kaesar <anditakaesar@gmail.com>
2017-09-12 19:36:26 +0700

    Removing blog app
    Removing blog app in settings.py

--- aoms/settings.py
+++ aoms/settings.py
@@ -32,7 +32,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
     'aomswork',
     'rest_framework',
     'corsheaders',
